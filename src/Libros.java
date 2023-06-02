8270fd9c2ef008f375d2175e469f5cd4770387b1
DennisCatana <dennis.joel235@outlook.es>
2023-06-02 12:37:56 -0500

    Prueba 1

--- src/Libros.java
+++ src/Libros.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Libros {
+public class Libros {
+    String nombre;
+    String autor;
+    int edicion;
+    int fecha_P;
+    int valor;
+
+    //Setter y getters
+    public String getNombre() {
+        return getNombre();
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(int edicion) {
+        this.edicion = edicion;
+    }
+
+    public int getFecha_P() {
+        return fecha_P;
+    }
+
+    public void setFecha_P(int fecha_P) {
+        this.fecha_P = fecha_P;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+public void imprimir(){
+
 }
+//Constructor
+    public Libros(String nombre, String autor, int edicion, int fecha_P, int valor) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.edicion = edicion;
+        this.fecha_P = fecha_P;
+        this.valor = valor;
+
+    }
+
+}
+
+
+
+
+
+
+
+
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Menu {
+public class Menu {
+    public static void main(String[] args) {
+        Libros libro1 = new Libros("La vaca Lola", "Alan Po", 5, 2010, 10);
+        Libros libro2 = new Libros("Don Quijote", "Agusto Valentine", 8, 2000, 50);
+        Libros libro3 = new Libros("El bien y el mal", "Alberto Cruz", 7, 2014, 45);
+        Libros libro4 = new Libros("La inocente", "Mora", 1, 2021, 5);
+        Libros libro5 = new Libros("Donde esta la Esperanza", "Cataña Dennis", 9, 2000, 2);
+
+
+        System.out.println("A continuacion datos del libro 1: ");
+       //libro1.imprimir_Datos();
+        System.out.println("A continuacion datos del libro 2: ");
+       // libro2.imprimir_Datos();
+        System.out.println("A continuacion datos del libro 3: ");
+        //libro3.imprimir_Datos();
+        System.out.println("A continuacion datos del libro 4: ");
+        //libro4.imprimir_Datos();
+        System.out.println("A continuacion datos del libro 5: ");
+        //libro5.imprimir_Datos();
+
+    }
 }
